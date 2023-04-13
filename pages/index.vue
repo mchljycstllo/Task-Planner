f6fcfafccbf0a38536f0265b142e3c4adb491d23
mjaycastillo <michael.castillo@adish.com.ph>
2023-04-14 05:28:43 +0800

    added swal emit listener to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,14 @@
   export default {
     components: {
       PageContent: () => import('~/components/home/PageContent')
+    },
+    mounted () {
+      this.$nuxt.$on('display-swal', (swal_payload) => {
+        this.$swal(swal_payload)
+      })
+    },
+    destroyed () {
+      this.$nuxt.$off('display-swal')
     }
   }
 </script>
